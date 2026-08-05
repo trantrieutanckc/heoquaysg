5aaed122c96c052c22a5585c3d62863e7bbd526d
trantrieutanckc <trantrieutanckc@gmail.com>
2026-08-05 20:34:26 +0700

    fix: kill port 3000 trước khi start để tránh EADDRINUSE

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
   apps: [
     {
       name: "heoquaybinhtan",
-      script: "node_modules/.bin/next",
-      args: "start",
+      script: "start.sh",
+      interpreter: "/bin/bash",
       cwd: "/home/heoquaybinhtan/app",
       instances: 1,
       exec_mode: "fork",
